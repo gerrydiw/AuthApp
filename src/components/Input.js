d13c4fc6d8ce50b8418c2d487b1670140e7ace32
gerrydiw <gerrydiw123@gmail.com>
2020-10-07 11:14:00 +0700

    checkpoint 6

--- src/components/Input.js
+++ src/components/Input.js
@@ -29,7 +29,7 @@ const style = StyleSheet.create({
 
 class Input extends Component {
     render() {
-        const { label, placeholder, secureTextEntry } = this.props
+        const { label, placeholder, secureTextEntry, value, onChangeText } = this.props
         return (
             <View style={ style.container }>
                 <Text style={ style.label }>{ label }</Text>
@@ -37,7 +37,9 @@ class Input extends Component {
                 style={ style.label }
                 placeholder={ placeholder }
                 secureTextEntry= { secureTextEntry }
-                autoCorrect= { false }/>
+                autoCorrect= { false }
+                value={ value }
+                onChangeText= { onChangeText }/>
             </View>
         );
     }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 
 import Card from './Card';
 import CardSection from './CardSection';
@@ -6,17 +8,51 @@ import Button from './Button';
 import Input from './Input';
 
 class LoginForm extends Component {
+    state = {
+        email: '',
+        password: '',
+        error: ''
+    }
+    onButtonPress() {
+        const {email, password } = this.state
+        console.log('Processing login')
+
+        this.setState({ error: '' })
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log(error.message);
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                .catch(( error2 )=> {
+                    console.log(error2.message);
+                    this.setState({ error: 'Authentication Failed'})
+                })
+            })
+    }
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Input label="Email" placeholder="user@gmail.com"/>
+                    <Input 
+                    label="Email" 
+                    placeholder="user@gmail.com"
+                    value={ this.state.email }
+                    onChangeText= { (email) => this.setState({ email })}/>
                 </CardSection>
                 <CardSection>
-                    <Input label="Password" placeholder="password" secureTextEntry={ true } />
+                    <Input 
+                    label="Password" 
+                    placeholder="password" 
+                    secureTextEntry={ true } 
+                    value={ this.state.password }
+                    onChangeText= { (password) => this.setState({ password })}/>
                 </CardSection>
+                <Text style={ { fontSize: 20, alignSelf: 'center', color: 'red'} }>
+                    { this.state.error }
+                </Text>
                 <CardSection>
-                    <Button>
+                    <Button 
+                    onPress={ () => this.onButtonPress() }>
                         Login
                     </Button>
                 </CardSection>
